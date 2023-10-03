fc794ecb345835ae8dc58632013f62811060c41c
sjnprjl <sujan.official57@gmail.com>
2023-10-03 23:18:47 +0545

    use: yargs for command line

--- index.ts
+++ index.ts
@@ -1,5 +1,22 @@
+import yargs from "yargs";
+
 import { Repl } from "./src/repl.ts";
+import { hideBin } from "./node_modules/yargs/build/lib/utils/process-argv.js";
 
-const repl = new Repl();
+const commands = yargs(hideBin(process.argv))
+  .scriptName("slang")
+  .command("<file>", "Slang interpreter", (yargs) => {
+    yargs.positional("file", {
+      describe: "file to interpret",
+      type: "string",
+    });
+  })
+  .help()
+  .parse();
 
-repl.run();
+const file = commands["_"][0];
+
+if (!file) {
+  const repl = new Repl();
+  repl.run();
+}
