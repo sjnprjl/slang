40a56a0a21e61577218becf9a798c5e855fd2a46
sjnprjl <sujan.official57@gmail.com>
2023-10-03 13:03:59 +0545

    tokenize mod operator

--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -183,6 +183,9 @@ export class Tokenizer {
       if (this.current === ".") {
         return this.createToken(TokenType.dot, this.eat);
       }
+      if (this.current === "%") {
+        return this.createToken(TokenType.mod, this.eat);
+      }
 
       if (this.isidentinitial) return this.tokenizeId();
 
